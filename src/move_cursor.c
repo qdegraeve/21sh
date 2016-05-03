c323ec2669ceb2bc0fa16c04f95d131eaa0ad470
Quentin DEGRAEVE <qdegraev@e2r4p20.42.fr>
2016-05-03 11:08:06 +0200

    save home end button

--- src/move_cursor.c
+++ src/move_cursor.c
@@ -3,18 +3,27 @@
 void move_cursor_line(t_env *e, int input)
 {
 	int		diff;
+	int		move;
 
 	diff = e->curs_max;
-	if (input == HOME)
+	move = 0;
+	if (input == HOME && e->curs_pos)
 	{
-			tputs(tgetstr("rc", NULL), 0, ft_putchar2);
-			tputs(tgetstr("sc", NULL), 0, ft_putchar2);
-			e->curs_pos = 0;
+		if ((move = (e->curs_pos + e->prompt_len) / e->width))
+			tputs(tgoto(tgetstr("UP", NULL), 0, move), 0, ft_putchar2);
+		move = ((e->curs_pos + e->prompt_len) % e->width) - e->prompt_len;
+		tputs(tgoto(tgetstr("LE", NULL), 0, move), 0, ft_putchar2);
+		tputs(tgetstr("sc", NULL), 0, ft_putchar2);
+		e->curs_pos = 0;
 	}
 	else if (input == END && e->curs_max - e->curs_pos > 0)
 	{
-			tputs(tgoto(tgetstr("RI", NULL), 0, e->curs_max -e->curs_pos), 0, ft_putchar2);
-			e->curs_pos = e->curs_max;
+		move = (e->curs_pos + e->prompt_len) / e->width;
+		if ((move = ((e->curs_max + e->prompt_len) / e->width) - move))
+			tputs(tgoto(tgetstr("DO", NULL), 0, move), 0, ft_putchar2);
+		move = ((e->curs_max + e->prompt_len) % e->width) - ((e->curs_pos + e->prompt_len) % e->width);
+		tputs(tgoto(tgetstr("RI", NULL), 0, move), 0, ft_putchar2);
+		e->curs_pos = e->curs_max;
 	}
 }
 
