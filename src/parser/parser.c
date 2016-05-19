6d6c98fa01d4cdcdeb766288dc7ac180e858c6c9
macbook air <macbookair@MacBook-Air-de-macbook.local>
2016-05-20 01:55:51 +0200

    fucking quote

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -78,18 +78,15 @@ void	parser(t_cmds **root, t_builtin *b)
 		replace_dollar(&tmp->cmd, b);
 		init_builtin(b, tmp->cmd);
 		clean_quote(b->argv);
-		//	ft_print_tab(my_cli.cmd);
 		if (tmp->input)
 		{
 			my_cli.input= str_to_argv(tmp->input);
 			clean_quote(my_cli.input);
-		//		ft_print_tab(my_cli.input);
 		}
 		if (tmp->output)
 		{
 			my_cli.output = str_to_argv(tmp->output);
 			clean_quote(my_cli.output);
-			//	ft_print_tab(my_cli.output);
 		}
 		get_command(b->argv[0], b);
 		if (b->path && tmp->pipe == 0)
